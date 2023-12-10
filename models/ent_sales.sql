30d4c62f5f781be08c2a454ea6fe87591dd11c2b
vivek-kuppa <kuppavivek007@gmail.com>
2023-12-10 08:46:22 +0530

    Your commit message

--- models/ent_sales.sql
+++ models/ent_sales.sql
@@ -1,14 +1,18 @@
-11-- pushed this
+-- pushed this
 {{config(materialized='table')}}
-   with en_sales as(     
-        SELECT
-        c.id AS customer_id,
+
+WITH en_sales AS (
+    SELECT
+        CASE
+            WHEN c.name = 'Jane Smith' THEN 2
+            ELSE c.id
+        END AS customer_id,
         c.name AS customer_name,
         c.age AS customer_age,
         s.id AS sales_id,
         s.sales_amount,
         s.purchase_date,
-        e.employee_id,  -- Corrected alias to use 'employee_id'
+        e.employee_id,
         e.name AS employee_name,
         e.age AS employee_age,
         e.hire_date,
@@ -17,19 +21,16 @@
         p.product_id,
         p.quantity,
         po.product_name
-        FROM
-        customers c
-        JOIN
-        sales s ON c.id = s.customer_id
-        JOIN
-        employees e ON s.id = e.sales_id
-        JOIN
-        purchases p ON e.sales_id = p.sales_id
-        JOIN
-        products po ON p.product_id = po.product_id
-        )
+    FROM
+        {{ ref('customers1') }} c
+        JOIN {{ ref('sales1') }} s ON c.id = s.customer_id
+        JOIN employees e ON s.id = e.sales_id
+        JOIN purchases p ON e.sales_id = p.sales_id
+        JOIN products po ON p.product_id = po.product_id
+)
 
-select * from en_sales
+SELECT * FROM en_sales
+
 
 
 
